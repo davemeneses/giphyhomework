e62fe4afb7fa96ea04ad3f740a9f5c147fea489b
davemeneses <meneses.coffee@gmail.com>
2019-03-13 17:14:28 -0500

    commented out the work

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  //Array for searched sports to be added
+  //this is the array I push all the things users search. I pre-populated it with 6.
   var sports = [
     "skateboarding",
     "rugby",
@@ -8,8 +8,9 @@ $(document).ready(function() {
     "cricket",
     "fencing"
   ];
-
-  function displayGIFS() {
+  // this is the function I built that actually hits the giphy API and searches the database
+  function createGifs() {
+    // this is the variable that takes user input and is slotted into the queryURL to search that specific topic.
     var sport = $(this).data("search");
     console.log(sport);
 
@@ -18,8 +19,7 @@ $(document).ready(function() {
       sport +
       "&api_key=G91ywVKl5sMotDMcwf8dBLNwtldkmzpm&limit=10";
 
-    // console.log(queryURL);
-
+    //this is what accesses the ipgy api and pulls the information we want
     $.ajax({
       url: queryURL,
       method: "GET"
@@ -27,16 +27,17 @@ $(document).ready(function() {
       var results = response.data;
       console.log(results);
       for (var i = 0; i < results.length; i++) {
+        // this is a bank of variables I use to do multiple things: 1) create a div for an individual gif 2)grabs the animated and still URLs from giphy, 3) adds the img tag, 4) pulls the rating for each gif, 5) adds a <p> tag and displays the gif rating
         var gifDiv = $("<div>");
-
         var animated = results[i].images.fixed_height.url;
         var still = results[i].images.fixed_height_still.url;
         var image = $("<img>");
         var rating = results[i].rating;
         var p = $("<p>").text("Rating: " + rating);
-
+        //all these commands take the variables above and attach them to each item in the sports array.
+        // this also set's me up to play/pause my gifs by adding the gif's state when it is created.
         image.attr("src", still);
-        image.addClass("Giphy");
+        image.attr("class", "Giphy");
         image.attr("data-state", "still");
         image.attr("data-still", still);
         image.attr("data-animate", animated);
@@ -46,20 +47,21 @@ $(document).ready(function() {
       }
     });
   }
-  //Submit button click event takes search term from form input, trims and pushes to sports array, displays button
+  //this is how the user searches new gifs.
   $("#submitButton").on("click", function(event) {
     event.preventDefault();
     var sportSearch = $("#searchArea")
+      // .val() actually grabs the things the user searched and .trim() gets ride of all spaces
       .val()
       .trim();
     sports.push(sportSearch);
     console.log(sports);
     $("#searchArea").val("");
-    displayButtons();
+    createButtons();
   });
 
-  //Function iterates through sports array to display button with array values in "buttons" section of HTML
-  function displayButtons() {
+  //this function actually creates the buttons from the key word (or words) that the user searched
+  function createButtons() {
     $("#buttons").empty();
     for (var i = 0; i < sports.length; i++) {
       var y = $('<button class="btn btn-secondary btn-sm">');
@@ -69,15 +71,17 @@ $(document).ready(function() {
       $("#buttons").append(y);
     }
   }
-  displayButtons();
-  displayGIFS();
+  // i call it right after i define it so it creates buttons for all the things I pre-populated the sports array with.
+  createButtons();
 
-  $(document).on("click", "#sport", displayGIFS);
+  // this is where I tell the JS how to interpret user clicks
+  $(document).on("click", "#sport", createGifs);
 
-  $(document).on("click", ".Giphy", pausePlayGifs);
+  // this is how I pause/play the gifs
+  $(document).on("click", ".Giphy", changeGIFState);
 
-  //   Function accesses "data-state" attribute and depending on status, changes image source to "data-animate" or "data-still"
-  function pausePlayGifs() {
+  //  Tom demo-ed this in class and it was very helpful. Was stuck on this for a LOOOOOONNNNGGGG time
+  function changeGIFState() {
     var state = $(this).attr("data-state");
     if (state === "still") {
       $(this).attr("src", $(this).attr("data-animate"));
